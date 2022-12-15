fb704477d6e64fa359c283834ec8e21788e268e1
nicaodev <alexandre.nicos@gmail.com>
2022-12-14 22:15:09 -0300

    poke list scss api

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +13,8 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
