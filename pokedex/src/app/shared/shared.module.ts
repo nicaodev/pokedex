a0eb7876a45563510dde93637f9ec48bba11ba4b
nicaodev <alexandre.nicos@gmail.com>
2022-12-15 22:29:41 -0300

    routerLink, grid

--- pokedex/src/app/shared/shared.module.ts
+++ pokedex/src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
+import { RouterLink } from '@angular/router';
 
 
 
@@ -14,7 +15,8 @@ import { PokeListComponent } from './poke-list/poke-list.component';
   ],
   exports: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink
   ]
 })
 export class SharedModule { }
